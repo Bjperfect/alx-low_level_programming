27172fdbfc0cf9ec2012e7b0586a7cec0d803a0a
shodimu bolaji <shodimubolaji@gmai.com>
2023-02-20 07:42:16 -0800

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,7 +9,7 @@ int main(void)
 {
 	int a;
 
-	for (a = 0; a < 100; a++)
+	for (a = 0; a < 10; a++)
 	{
 		putchar(a + '0');
 		if (a < 9)
@@ -21,4 +21,3 @@ int main(void)
 	putchar('\n');
 	return (0);
 }
-
