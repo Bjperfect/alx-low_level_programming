edaf2a07b5b3df43e7431065e650b986a70a95c6
shodimu bolaji <shodimubolaji@gmai.com>
2023-02-19 17:42:48 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+char c;
+
+c = '0';
+while
+	(c <= '9')
+		c++;
+{
+putchar(c);
+}
+putchar('\n');
+return (0);
+}
+
