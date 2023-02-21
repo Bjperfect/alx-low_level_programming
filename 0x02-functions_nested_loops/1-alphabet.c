98341559ec55de355ca857f0be2fbe129c9f7d84
shodimu bolaji <shodimubolaji@gmai.com>
2023-02-21 11:11:46 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *print_alphabet - print all alphabelt in lowercase
+ */
+
+void print_alphabet(void)
+
+{
+	char lowercase;
+
+	for (lowercass = 'a'; lowercase <= 'z'; lowercase++)
+	_putchar(lowercase);
+
+	_putchar('\n');
+}
