a8168a6c5f37c9fb9aba1d3e84a401dfd6750e0b
shodimu bolaji <shodimubolaji@gmai.com>
2023-02-21 04:58:24 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,14 +2,14 @@
 
 /**
  * main - print _putchar
- * is written by bjperfect
+ * is written by bwave
  * Return: 0
  */
 
 int main(void)
 {
 _putchar('_');
-_putchar('p');|
+_putchar('p');
 _putchar('u');
 _putchar('t');
 _putchar('c');
