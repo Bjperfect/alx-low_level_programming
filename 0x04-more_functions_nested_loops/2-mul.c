5c208cf8188d2e2a15e40ec3b7bcc72fe69674ff
shodimu bolaji <shodimubolaji@gmai.com>
2023-02-23 17:29:40 -0800

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - funtion that multiply two int
+ * @a: one of the int
+ * @b: second int
+ * Return: multiplicastion result
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
