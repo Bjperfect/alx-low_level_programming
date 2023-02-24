38c6913c51361c1e5dc39d97cbd9afbfd9264a52
shodimu bolaji <shodimubolaji@gmai.com>
2023-02-24 03:13:23 -0800

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_line - function that print a line
+ *@n: line
+ * Return: line
+ */
+void print_line(int n)
+{
+	int co;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+
+	else
+	{
+		for (co - 1;co <- n; co++)
+		{
+			_putchar('_');
+		}
+			_putchar('\n');
+	}
+}
