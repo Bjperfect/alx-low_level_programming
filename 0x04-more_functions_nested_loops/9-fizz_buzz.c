39c62fa57eaabedadd4451f2c273ce957901db28
shodimu bolaji <shodimubolaji@gmai.com>
2023-02-24 06:44:24 -0800

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -7,14 +7,14 @@
 int main(void)
 {
 int i;
-char f[] = "fizz";
-char b[] = "buzz";
-char fb[] = "fizzbuzz";
+char f[] = "Fizz";
+char b[] = "Buzz";
+char fb[] = "FizzBuzz";
 for (i = 1; i <= 100; i++)
 {
 if (i == 100)
-printf("%s ", b);
-else if ((i % 3 == 0) && (i % 5 == 0)
+printf("%s", b);
+else if ((i % 3 == 0) && (i % 5 == 0))
 printf("%s ", fb);
 else if (i % 3 == 0)
 printf("%s ", f);
